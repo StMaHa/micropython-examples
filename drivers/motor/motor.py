9fc6353f343c8f077af19618786d17f932645116
SMH <github@inca-vikingo.de>
2022-06-11 14:41:46 +0200

    Change line endings to unix style

--- drivers/motor/motor.py
+++ drivers/motor/motor.py
@@ -1,1 +1,37 @@
-from machine import Pin, PWMclass Motor:          def __init__(self, pin1, pin2, pwm=False):        self.max_duty_cycle = 1023        self.pwm = pwm        if self.pwm:            self.pin1 = PWM(Pin(pin1), freq=500, duty=0)            self.pin2 = PWM(Pin(pin2), freq=500, duty=0)        else:            self.pin1 = Pin(pin1, Pin.OUT)            self.pin2 = Pin(pin2, Pin.OUT)    def forward(self, speed = 1):        if self.pwm:            self.pin1.duty(0)            self.pin2.duty(int(self.max_duty_cycle * speed))        else:            self.pin1.off()            self.pin2.on()        def backward(self, speed = 1):        if self.pwm:            self.pin1.duty(int(self.max_duty_cycle * speed))            self.pin2.duty(0)        else:            self.pin1.on()            self.pin2.off()    def stop(self):        if self.pwm:            self.pin1.duty(0)            self.pin2.duty(0)        else:                    self.pin1.off()            self.pin2.off()
+from machine import Pin, PWM
+
+
+class Motor:      
+    def __init__(self, pin1, pin2, pwm=False):
+        self.max_duty_cycle = 1023
+        self.pwm = pwm
+        if self.pwm:
+            self.pin1 = PWM(Pin(pin1), freq=500, duty=0)
+            self.pin2 = PWM(Pin(pin2), freq=500, duty=0)
+        else:
+            self.pin1 = Pin(pin1, Pin.OUT)
+            self.pin2 = Pin(pin2, Pin.OUT)
+
+    def forward(self, speed = 1):
+        if self.pwm:
+            self.pin1.duty(0)
+            self.pin2.duty(int(self.max_duty_cycle * speed))
+        else:
+            self.pin1.off()
+            self.pin2.on()
+    
+    def backward(self, speed = 1):
+        if self.pwm:
+            self.pin1.duty(int(self.max_duty_cycle * speed))
+            self.pin2.duty(0)
+        else:
+            self.pin1.on()
+            self.pin2.off()
+
+    def stop(self):
+        if self.pwm:
+            self.pin1.duty(0)
+            self.pin2.duty(0)
+        else:        
+            self.pin1.off()
+            self.pin2.off()
--- projects/robo_car.py
+++ projects/robo_car.py
@@ -1,1 +1,91 @@
-# Aufgaben# +++ 1) Messen mit dem Ultraschallentfernungssensor „HC-SR04“ in einer Dauerschleife# +++ 2) Start / Stop der Motoren in Abhängigkeit der Entfernung# +++ 3) Zufälliges drehen des Robos nach links oder rechts# Bibliotheken und Klassenfrom machine import Pinfrom time import sleepfrom random import choicefrom hcsr04 import HCSR04from motor import Motor# Globale Variablen# Motorenleistungspeed_m1 = 0.5speed_m2 = 0.5# Pinnummern beziehen sich auf Wemos S2 mini# GPIOs zur Ansteuerung der Motorenpin_m1a = 37  # motor 1pin_m1b = 38  # motor 1pin_m2a = 39  # motor 2pin_m2b = 40  # motor 2# GPIOs des Abstandssensorspin_trigger = 17pin_echo = 16# Hier bgeinnt das Hauptprogrammdef start():    # Erzeuge eine Instanz der Motor-Klasse    # aktivieren von PWM    motor1 = Motor(pin_m1a, pin_m1b, pwm=True)    motor2 = Motor(pin_m2a, pin_m2b, pwm=True)    # Erzeuge eine Instanz der Distanzsensor-Klasse    sensor = HCSR04(trigger_pin=pin_trigger, echo_pin=pin_echo, echo_timeout_us=10000)    # Try-Catch-Block    try:        # Schleife endlos durchlaufen        # +++ 1) +++        while True:            # Schreibe Sensormesswert (cm) auf die Konsole (kann spaeter wieder auskommentiert werden)            # +++ 2) +++ Auskommentieren            #print(sensor.distance_cm())            # Ist der Sensormesswert (cm) kleiner 40 cm?            # +++ 2) +++            # messe Entfernung            if sensor.distance_cm() < 40:                # alle Motoren stop                motor1.stop()                motor2.stop()                # eine 1/2 Sekunde warten                sleep(0.5)                # +++ 3) +++                # Waehle zufaellig eine Drehrichtung                if choice([0, 1]):                    # Robo dreht in die eine Richtung                    motor1.forward(speed_m1)                    motor2.backward(speed_m2)                else:                    # Robo dreht in die andere Richtung                    motor1.backward(speed_m1)                    motor2.forward(speed_m2)                # Lass den Robo eine 1/2 Sekunde drehen                sleep(0.5)                # alle Motoren stop                motor1.stop()                motor2.stop()                # eine 1/2 Sekunde warten                sleep(0.5)            else:                # fahre Robo vorwaerts                motor1.forward(speed_m1)                motor2.forward(speed_m2)    # Fangen eines Fehlers/Signals    except KeyboardInterrupt:        print("Programm abgebrochen.")    # Dieser Block wird immer ausgefuehrt: zum Schluss muss man aufraeumen    finally:        # Motoren aus        motor1.stop()        motor2.stop()        print("Programm beendet.")# Manuelles Ausführen in der IDEif __name__ == '__main__':    start()
+# Aufgaben
+# +++ 1) Messen mit dem Ultraschallentfernungssensor „HC-SR04“ in einer Dauerschleife
+# +++ 2) Start / Stop der Motoren in Abhängigkeit der Entfernung
+# +++ 3) Zufälliges drehen des Robos nach links oder rechts
+
+# Bibliotheken und Klassen
+from machine import Pin
+from time import sleep
+from random import choice
+
+from hcsr04 import HCSR04
+from motor import Motor
+
+# Globale Variablen
+
+# Motorenleistung
+speed_m1 = 0.5
+speed_m2 = 0.5
+
+# Pinnummern beziehen sich auf Wemos S2 mini
+# GPIOs zur Ansteuerung der Motoren
+pin_m1a = 37  # motor 1
+pin_m1b = 38  # motor 1
+pin_m2a = 39  # motor 2
+pin_m2b = 40  # motor 2
+# GPIOs des Abstandssensors
+pin_trigger = 17
+pin_echo = 16
+
+# Hier bgeinnt das Hauptprogramm
+def start():
+    # Erzeuge eine Instanz der Motor-Klasse
+    # aktivieren von PWM
+    motor1 = Motor(pin_m1a, pin_m1b, pwm=True)
+    motor2 = Motor(pin_m2a, pin_m2b, pwm=True)
+
+    # Erzeuge eine Instanz der Distanzsensor-Klasse
+    sensor = HCSR04(trigger_pin=pin_trigger, echo_pin=pin_echo, echo_timeout_us=10000)
+
+    # Try-Catch-Block
+    try:
+        # Schleife endlos durchlaufen
+        # +++ 1) +++
+        while True:
+            # Schreibe Sensormesswert (cm) auf die Konsole (kann spaeter wieder auskommentiert werden)
+            # +++ 2) +++ Auskommentieren
+            #print(sensor.distance_cm())
+
+            # Ist der Sensormesswert (cm) kleiner 40 cm?
+            # +++ 2) +++
+            # messe Entfernung
+            if sensor.distance_cm() < 40:
+                # alle Motoren stop
+                motor1.stop()
+                motor2.stop()
+                # eine 1/2 Sekunde warten
+                sleep(0.5)
+                # +++ 3) +++
+                # Waehle zufaellig eine Drehrichtung
+                if choice([0, 1]):
+                    # Robo dreht in die eine Richtung
+                    motor1.forward(speed_m1)
+                    motor2.backward(speed_m2)
+                else:
+                    # Robo dreht in die andere Richtung
+                    motor1.backward(speed_m1)
+                    motor2.forward(speed_m2)
+                # Lass den Robo eine 1/2 Sekunde drehen
+                sleep(0.5)
+                # alle Motoren stop
+                motor1.stop()
+                motor2.stop()
+                # eine 1/2 Sekunde warten
+                sleep(0.5)
+            else:
+                # fahre Robo vorwaerts
+                motor1.forward(speed_m1)
+                motor2.forward(speed_m2)
+    # Fangen eines Fehlers/Signals
+    except KeyboardInterrupt:
+        print("Programm abgebrochen.")
+    # Dieser Block wird immer ausgefuehrt: zum Schluss muss man aufraeumen
+    finally:
+        # Motoren aus
+        motor1.stop()
+        motor2.stop()
+        print("Programm beendet.")
+
+# Manuelles Ausführen in der IDE
+if __name__ == '__main__':
+    start()
