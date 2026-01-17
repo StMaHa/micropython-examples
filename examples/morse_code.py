09dbcc18e4e48da33f6510c3aa5446f5e15e5ef2
stmaha <stmaha@inca-vikingo.de>
2026-01-17 20:56:41 +0100

    Fix morse code example

--- examples/morse_code.py
+++ examples/morse_code.py
@@ -1,10 +1,12 @@
 # Simple example for morse code by flashing a LED
 from machine import Pin
+from time import sleep
 
-led = Pin("LED")
+led = Pin("LED", Pin.OUT)
 
-MORSE_CODE_SHORT = 0.33  # seconds
-MORSE_CODE_LONG = MORSE_CODE_SHORT * 3  # seconds
+MORSE_CODE_SHORT_DELAY = 0.33  # seconds
+MORSE_CODE_LONG_DELAY = MORSE_CODE_SHORT_DELAY * 3  # seconds
+MORCE_CODE_WORD_DELAY = MORSE_CODE_SHORT_DELAY * 6  # seconds
 
 morse_code_dict = {
     'A': '.-',
@@ -49,28 +51,33 @@ morse_code_dict = {
     '?': '..--..',
     ';': '-.-.-.',
     ':': '---...',
-    "'", '.----.',
+    "'": '.----.',
     '-': '-....-',
     '/': '-..-.',
     '': '-.--.-',
     ')': '-.--.-',
     '_': '..--.-'
 }
-
-text = input("Morse code: ")
-while True:  # Endlosschleife
-    for character in text.higher():
-        code = morse_code_dict[character]
-        if code != ' ':
-            if code == '.':
-                morse_length = MORSE_CODE_SHORT
-            else:
-                morse_length = MORSE_CODE_LONG
-            led.on()
-            sleep(morse_length)
-            led.off()
-            sleep(MORSE_CODE_SHORT)
-        else:
-            sleep(MORSE_CODE_SHORT)
-    # delay between characters
-    sleep(MORSE_CODE_SHORT)
+text = input("Enter text to morse: ")  # e.g. sos
+try:
+    while True:  # Endlosschleife
+        for character in text:
+            morse_code = morse_code_dict[character.upper()]
+            for code in morse_code:
+                if code == ' ':  # White space, e.g. between words
+                    sleep(MORSE_CODE_SHORT_DELEAY)
+                    continue
+                elif code == '.':
+                    morse_length = MORSE_CODE_SHORT_DELAY
+                elif code == "-":
+                    morse_length = MORSE_CODE_LONG_DELAY
+                else:
+                   continue
+                led.on()
+                sleep(morse_length)
+                led.off()
+                sleep(MORSE_CODE_SHORT_DELAY)
+        sleep(MORSE_CODE_LONG_DELAY)  # Delay after text send.
+except KeyboardInterrupt:
+    print("Programmende")
+    led.off()
